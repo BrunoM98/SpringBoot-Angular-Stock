d8aefb1815acb0aaa027cc7b459aba247429e201
Bruno Mazziero <bruno.mazziero98@gmail.com>
2026-01-28 13:33:03 -0300

    reactorizacion de porduct-list para el funcionamiento correcto con java\Angular

--- inventary-app/src/app/product-list/product-list.ts
+++ inventary-app/src/app/product-list/product-list.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core'; // <--- Añadimos signal
+import { Product } from '../product';
+import { ProductService } from '../product-service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-list',
-  imports: [],
-  templateUrl: './product-list.html'})
-export class ProductList {
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './product-list.html'
+})
+export class ProductList implements OnInit {
+  
+  // 1. Ahora es un Signal. Esto "avisa" al HTML automáticamente.
+  products = signal<Product[]>([]); 
 
+  private productService = inject(ProductService);
+
+  ngOnInit() {
+    this.obteinProduct();
+  }
+
+  private obteinProduct(): void {
+    this.productService.obteinListProduct().subscribe({
+      next: (data) => {
+        console.log("¡Llegaron los datos!", data);
+        // 2. Para actualizar un signal usamos .set()
+        this.products.set(data); 
+      },
+      error: (error) => console.error("Error al obtener datos:", error)
+    });
+  }
 }
